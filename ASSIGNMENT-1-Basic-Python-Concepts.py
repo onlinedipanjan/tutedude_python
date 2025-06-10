4cb5acb842ae8d67436fa33ed872e689f5b134e6
onlinedipanjan <77239221+onlinedipanjan@users.noreply.github.com>
2025-06-10 22:36:58 +0530

    Add files via upload

---
+++ ASSIGNMENT-1-Basic-Python-Concepts.py
@@ -0,0 +1,45 @@
+# Task 1: Perform Basic Mathematical Operations
+# Problem Statement: Write a Python program that does the following:
+# 1.  Takes two numbers as input from the user.
+# 2.  Performs the basic mathematical operations on these two numbers:
+# o	Addition
+# o	Subtraction
+# o	Multiplication
+# o	Division
+# 3.  Displays the results of each operation on the screen.
+
+# answer
+
+a=int(input("Enter the first Number:- "))
+b=int(input("Enter the Second Number:- "))
+
+Addition1 = a + b
+Subtraction1 =  a -b
+Multiplication1 = a * b
+Division1 = a / b
+
+print("Addition ", Addition1)
+print("Subtraction ", Subtraction1)
+print("Multiplication ", Multiplication1)
+print("Division ", Division1)
+
+
+
+# Task 2: Create a Personalized Greeting
+# Problem Statement: Write a Python program that:
+# 1.  Takes a user's first name and last name as input.
+# 2.  Concatenates the first name and last name into a full name.
+# 3.  Prints a personalized greeting message using the full name.
+
+
+# answer
+
+
+firstname=input("Enter the first Name:- ")
+lastname=input("Enter the Second Name:-")
+join1 = firstname + " " +lastname+"!"
+
+print("Hello,",join1,"Welcome to the python program" )
+
+
+
